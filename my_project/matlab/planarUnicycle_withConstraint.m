df129be6231be1275e4a8049618cac9bc7b30f6b
camilo-ordonez <cordonez@fsu.edu>
2023-04-19 17:49:59 -0400

    added files to simulate unicycle dynamics

---
+++ my_project/matlab/planarUnicycle_withConstraint.m
@@ -0,0 +1,47 @@
+% planar vehicle dynamic model. 
+% Unicycle with zero side slip velocity. 
+
+clear all
+close all
+clc
+
+%params = getparams();
+
+params.m = 10;
+params.I = 10;
+
+m = params.m;
+I = params.I;
+
+%q = [X Y th dX dY dth vx]; 
+
+%q = [X Y th dX dY dth vx vy]; 
+
+
+q0 = [0 0 0 1 0 0.2 1 0];
+tspan = [0:0.01:1];
+
+[t,q] = ode45(@(t,q)myUnicycleWithConstraint(t,q,params),tspan,q0);
+
+figure(1)
+
+    plot(q(:,1),q(:,2))
+    xlabel('time (s)')
+    ylabel('fwd vel (m/s')
+
+    axis equal
+    grid on
+
+figure(2)
+
+    plot(t,q(:,7))
+    xlabel('time (s)')
+    ylabel('fwd vel (m/s')
+
+    axis equal
+    grid on
+
+
+
+
+
